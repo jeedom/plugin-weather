cd00e4bc52133ef021221603ba53e9092db6af47
loic <lgevrey@oxya.com>
2015-04-13 13:44:38 +0200

    UI improvement

--- desktop/php/weather.php
+++ desktop/php/weather.php
@@ -1,6 +1,6 @@
 <?php
 if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
+	throw new Exception('{{401 - Accès non autorisé}}');
 }
 sendVarToJS('eqType', 'weather');
 $eqLogics = eqLogic::byType('weather');
@@ -13,10 +13,10 @@ $eqLogics = eqLogic::byType('weather');
                 <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter une météo}}</a>
                 <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
                 <?php
-                foreach ($eqLogics as $eqLogic) {
-                    echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
-                }
-                ?>
+foreach ($eqLogics as $eqLogic) {
+	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+}
+?>
             </ul>
         </div>
     </div>
@@ -24,115 +24,115 @@ $eqLogics = eqLogic::byType('weather');
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
         <legend>{{Mes météos}}
         </legend>
+        <div class="eqLogicThumbnailContainer">
+          <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+             <center>
+                <i class="fa fa-plus-circle" style="font-size : 7em;color:#94ca02;"></i>
+            </center>
+            <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02"><center>Ajouter</center></span>
+        </div>
         <?php
-        if (count($eqLogics) == 0) {
-            echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de météo, cliquez sur Ajouter une météo pour commencer}}</span></center>";
-        } else {
-            ?>
-            <div class="eqLogicThumbnailContainer">
-                <?php
-                foreach ($eqLogics as $eqLogic) {
-                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
-                    echo "<center>";
-                    echo '<img src="plugins/weather/doc/images/weather_icon.png" height="105" width="95" />';
-                    echo "</center>";
-                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
-                    echo '</div>';
-                }
-                ?>
-            </div>
-            <?php } ?>
-        </div>
-
-        <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-            <form class="form-horizontal">
-                <fieldset>
-                    <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">{{Nom de l'équipement météo}}</label>
-                        <div class="col-sm-3">
-                            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-                            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo}}"/>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-                        <div class="col-sm-3">
-                            <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-                                <option value="">{{Aucun}}</option>
-                                <?php
-                                foreach (object::all() as $object) {
-                                    echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label" >{{Activer}}</label>
-                        <div class="col-sm-1">
-                            <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" size="16" checked/>
-                        </div>
-                        <label class="col-sm-3 control-label" >{{Visible}}</label>
-                        <div class="col-sm-1">
-                            <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label">{{WOEID}}</label>
-                        <div class="col-sm-3">
-                            <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="city" placeholder="WOEID"/>
-                        </div>
-                        <div class="col-sm-3">
-                            <a class="btn btn-default" href="http://isithackday.com/geoplanet-explorer/" target=_blank>{{Obtenir le code WOEID}}</a>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label" >{{Historiser les données}}</label>
-                        <div class="col-sm-1">
-                            <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="historize" />
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label" >{{Affichage complet en mobile}}</label>
-                        <div class="col-sm-1">
-                            <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="fullMobileDisplay" />
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-sm-3 control-label" >{{Fréquence de rafraichissement des données (par défaut : */30 * * * *)}}</label>
-                        <div class="col-sm-2">
-                            <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="refreshCron"/>
-                        </div>
-                        <div class="col-sm-1">
-                            <i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
-                        </div>
-                    </div>
-                </fieldset> 
-            </form>
-
-            <legend>{{Météo}}</legend>
-            <table id="table_weather" class="table table-bordered table-condensed">
-                <thead>
-                    <tr>
-                        <th>{{Nom}}</th><th>{{Valeur}}</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
-
-            <form class="form-horizontal">
-                <fieldset>
-                    <div class="form-actions">
-                        <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-                        <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
-                    </div>
-                </fieldset>
-            </form>
-
-        </div>
+foreach ($eqLogics as $eqLogic) {
+	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+	echo "<center>";
+	echo '<img src="plugins/weather/doc/images/weather_icon.png" height="105" width="95" />';
+	echo "</center>";
+	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+	echo '</div>';
+}
+?>
     </div>
+</div>
 
-    <?php include_file('desktop', 'weather', 'js', 'weather'); ?>
-    <?php include_file('core', 'plugin.template', 'js'); ?>
+<div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+    <form class="form-horizontal">
+        <fieldset>
+            <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{Nom de l'équipement météo}}</label>
+                <div class="col-sm-3">
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                    <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo}}"/>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+                <div class="col-sm-3">
+                    <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+                        <option value="">{{Aucun}}</option>
+                        <?php
+foreach (object::all() as $object) {
+	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+}
+?>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Activer}}</label>
+                <div class="col-sm-1">
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" size="16" checked/>
+                </div>
+                <label class="col-sm-3 control-label" >{{Visible}}</label>
+                <div class="col-sm-1">
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label">{{WOEID}}</label>
+                <div class="col-sm-3">
+                    <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="city" placeholder="WOEID"/>
+                </div>
+                <div class="col-sm-3">
+                    <a class="btn btn-default" href="http://isithackday.com/geoplanet-explorer/" target=_blank>{{Obtenir le code WOEID}}</a>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Historiser les données}}</label>
+                <div class="col-sm-1">
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="historize" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Affichage complet en mobile}}</label>
+                <div class="col-sm-1">
+                    <input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="fullMobileDisplay" />
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-3 control-label" >{{Fréquence de rafraichissement des données (par défaut : */30 * * * *)}}</label>
+                <div class="col-sm-2">
+                    <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="refreshCron"/>
+                </div>
+                <div class="col-sm-1">
+                    <i class="fa fa-question-circle cursor floatright" id="bt_cronGenerator"></i>
+                </div>
+            </div>
+        </fieldset>
+    </form>
+
+    <legend>{{Météo}}</legend>
+    <table id="table_weather" class="table table-bordered table-condensed">
+        <thead>
+            <tr>
+                <th>{{Nom}}</th><th>{{Valeur}}</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+
+    <form class="form-horizontal">
+        <fieldset>
+            <div class="form-actions">
+                <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+                <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+            </div>
+        </fieldset>
+    </form>
+
+</div>
+</div>
+
+<?php include_file('desktop', 'weather', 'js', 'weather');?>
+<?php include_file('core', 'plugin.template', 'js');?>
