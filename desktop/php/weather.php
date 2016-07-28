ab40c4139138d86e2b92ad3f43207a44dd6780cd
Loïc <loic@jeedom.com>
2016-07-28 18:19:29 +0200

    Improve UI

--- desktop/php/weather.php
+++ desktop/php/weather.php
@@ -7,28 +7,28 @@ $eqLogics = eqLogic::byType('weather');
 ?>
 
 <div class="row row-overflow">
-    <div class="col-lg-2">
-        <div class="bs-sidebar">
-            <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
-                <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un dispositif}}</a>
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php
+  <div class="col-lg-2">
+    <div class="bs-sidebar">
+      <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+        <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fa fa-plus-circle"></i> {{Ajouter un dispositif}}</a>
+        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+        <?php
 foreach ($eqLogics as $eqLogic) {
 	echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
 }
 ?>
-            </ul>
-        </div>
-    </div>
-	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+     </ul>
+   </div>
+ </div>
+ <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
    <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
    <div class="eqLogicThumbnailContainer">
     <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
-           <center>
-            <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
-        </center>
-        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;;color:#94ca02"><center>{{Ajouter}}</center></span>
-    </div>
+     <center>
+      <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
+    </center>
+    <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;;color:#94ca02"><center>{{Ajouter}}</center></span>
+  </div>
   <div class="cursor eqLogicAction" data-action="gotoPluginConf" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
     <center>
       <i class="fa fa-wrench" style="font-size : 5em;color:#767676;"></i>
@@ -37,9 +37,9 @@ foreach ($eqLogics as $eqLogic) {
   </div>
 </div>
 <legend><i class="icon meteo-soleil"></i>  {{Mes Météos}}
-        </legend>
-        <div class="eqLogicThumbnailContainer">
-          <?php
+</legend>
+<div class="eqLogicThumbnailContainer">
+  <?php
 foreach ($eqLogics as $eqLogic) {
 	$opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
@@ -50,87 +50,92 @@ foreach ($eqLogics as $eqLogic) {
 	echo '</div>';
 }
 ?>
-            </div>
-    </div>
+</div>
+</div>
 
 <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
-  <form class="form-horizontal">
-    <fieldset>
-      <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
-      <div class="form-group">
-        <label class="col-sm-3 control-label">{{Nom de l'équipement météo}}</label>
-        <div class="col-sm-3">
-          <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
-          <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo}}"/>
+ <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+ <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
+
+ <ul class="nav nav-tabs" role="tablist">
+  <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
+  <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
+</ul>
+
+<div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+  <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+    <form class="form-horizontal">
+      <fieldset>
+        <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i> {{Général}}  <i class='fa fa-cogs eqLogicAction pull-right cursor expertModeVisible' data-action='configure'></i></legend>
+        <div class="form-group">
+          <label class="col-sm-3 control-label">{{Nom de l'équipement météo}}</label>
+          <div class="col-sm-3">
+            <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+            <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement météo}}"/>
+          </div>
         </div>
-      </div>
-      <div class="form-group">
-        <label class="col-sm-3 control-label" >{{Objet parent}}</label>
-        <div class="col-sm-3">
-          <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
-            <option value="">{{Aucun}}</option>
-            <?php
+        <div class="form-group">
+          <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+          <div class="col-sm-3">
+            <select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
+              <option value="">{{Aucun}}</option>
+              <?php
 foreach (object::all() as $object) {
 	echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
 }
 ?>
-         </select>
+           </select>
+         </div>
        </div>
-     </div>
-     <div class="form-group">
-      <label class="col-sm-3 control-label">{{Catégorie}}</label>
-      <div class="col-sm-6">
-        <?php
+       <div class="form-group">
+        <label class="col-sm-3 control-label">{{Catégorie}}</label>
+        <div class="col-sm-6">
+          <?php
 foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
 	echo '<label class="checkbox-inline">';
 	echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
 	echo '</label>';
 }
 ?>
+       </div>
      </div>
-   </div>
-  <div class="form-group">
-        <label class="col-sm-3 control-label"></label>
-        <div class="col-sm-9">
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
-            <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
-        </div>
+     <div class="form-group">
+      <label class="col-sm-3 control-label"></label>
+      <div class="col-sm-9">
+        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+      </div>
     </div>
- <div class="form-group">
-  <label class="col-sm-3 control-label">{{Ville}}</label>
-  <div class="col-sm-3">
-    <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="city" />
-  </div>
-</div>
-<div class="form-group">
-  <label class="col-sm-3 control-label" ></label>
-  <div class="col-sm-9">
-    <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="fullMobileDisplay" />{{Affichage complet en mobile}}</label>
-<label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="modeImage" />{{Mode image}}</label>
-  </div>
-</div>
-</fieldset>
-</form>
-<div class="alert alert-info">{{Si vous venez de créer votre clé api il faut peut être patienter quelques heures avant qu'elle soit active (en attendant il peut y avoir une erreur d'authentification)}} </div>
-<legend><i class="fa fa-list-alt"></i>  {{Météo}}</legend>
-<table id="table_cmd" class="table table-bordered table-condensed">
-  <thead>
-    <tr>
-      <th>{{Nom}}</th><th>{{Options}}</th><th></th>
-    </tr>
-  </thead>
-  <tbody>
-  </tbody>
-</table>
-
-<form class="form-horizontal">
-  <fieldset>
-    <div class="form-actions">
-      <a class="btn btn-danger eqLogicAction" data-action="remove"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>
-      <a class="btn btn-success eqLogicAction" data-action="save"><i class="fa fa-check-circle"></i> {{Sauvegarder}}</a>
+    <div class="form-group">
+      <label class="col-sm-3 control-label">{{Ville}}</label>
+      <div class="col-sm-3">
+        <input type="text" class="eqLogicAttr configuration form-control" data-l1key="configuration" data-l2key="city" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="col-sm-3 control-label" ></label>
+      <div class="col-sm-9">
+        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="fullMobileDisplay" />{{Affichage complet en mobile}}</label>
+        <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="configuration" data-l2key="modeImage" />{{Mode image}}</label>
+      </div>
     </div>
   </fieldset>
 </form>
+<div class="alert alert-info">{{Si vous venez de créer votre clé api il faut peut être patienter quelques heures avant qu'elle soit active (en attendant il peut y avoir une erreur d'authentification)}} </div>
+</div>
+<div role="tabpanel" class="tab-pane" id="commandtab">
+  <table id="table_cmd" class="table table-bordered table-condensed">
+    <thead>
+      <tr>
+        <th>{{Nom}}</th><th>{{Options}}</th><th></th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+
+</div>
+</div>
 
 </div>
 </div>
