7ff6f47c7cd961969e1715bdd403774dec918503
Loïc <loic@jeedom.com>
2017-12-12 17:47:07 +0100

    Add research bar

--- desktop/php/weather.php
+++ desktop/php/weather.php
@@ -22,7 +22,7 @@ foreach ($eqLogics as $eqLogic) {
    </div>
  </div>
  <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-   <legend><i class="fa fa-cog"></i>  {{Gestion}}</legend>
+   <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
    <div class="eqLogicThumbnailContainer">
     <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
       <i class="fa fa-plus-circle" style="font-size : 5em;color:#94ca02;"></i>
@@ -35,8 +35,8 @@ foreach ($eqLogics as $eqLogic) {
       <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676">{{Configuration}}</span>
     </div>
   </div>
-  <legend><i class="icon meteo-soleil"></i>  {{Mes Météos}}
-  </legend>
+  <legend><i class="icon meteo-soleil"></i> {{Mes Météos}}</legend>
+  <input class="form-control" placeholder="{{Rechercher}}" style="margin-bottom:4px;" id="in_searchEqlogic" />
   <div class="eqLogicThumbnailContainer">
     <?php
 foreach ($eqLogics as $eqLogic) {
@@ -44,7 +44,7 @@ foreach ($eqLogics as $eqLogic) {
 	echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
 	echo '<img src="' . $plugin->getPathImgIcon() . '" height="105" width="95" />';
 	echo "<br>";
-	echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
+	echo '<span class="name" style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;">' . $eqLogic->getHumanName(true, true) . '</span>';
 	echo '</div>';
 }
 ?>
