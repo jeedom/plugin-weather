2765ed9a271f5d00541d14b1b166de5125590f63
Loïc <loic@jeedom.com>
2023-04-24 10:27:33 +0200

    add more condition

--- core/class/weather.class.php
+++ core/class/weather.class.php
@@ -99,7 +99,7 @@ class weather extends eqLogic {
 		if (in_array($_condition_id, array(1135, 1030, 1072, 1147, 1150, 1153, 1168, 1171))) {
 			return 'meteo-brouillard';
 		}
-		if (in_array($_condition_id, array(1189, 1195, 1063, 1180, 1186, 1201, 1240, 1243, 1246,))) {
+		if (in_array($_condition_id, array(1189, 1195, 1063, 1180, 1186, 1201, 1240, 1243, 1246, 1183, 1207, 1198, 1192))) {
 			return 'meteo-pluie';
 		}
 		if (in_array($_condition_id, array(1066, 1069, 1114, 1117, 1204, 1210, 1213, 1216, 1219, 1222, 1225, 1237, 1249, 1252, 1255, 1258, 1261, 1264))) {
